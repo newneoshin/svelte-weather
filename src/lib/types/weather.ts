9fc9b39d6a5591e37605575fb7e441190f51ab2e
newneoshin <newneoshin@posteo.com>
2026-07-15 17:06:47 +0900

    feat: 시간별 날씨 API 응답 및 도메인 타입 정의
    아래의 타입을 정의.
    - HourlyWeatherApiResponse
    - HourlyWheatherItem
    - HourlyWeather

--- src/lib/types/weather.ts
+++ src/lib/types/weather.ts
@@ -7,9 +7,35 @@ export interface CurrentWeatherApiResponse {
   };
 }
 
+export interface HourlyWeatherApiResponse {
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+    wind_speed_2m: number[];
+    weather_code: number[];
+    relative_humidity_2m: number[];
+    uv_index: number[];
+    surface_pressure: number[];
+  };
+}
+
 // Processed
 
 export interface CurrentWeather {
   temperature: number;
   weather_code: number;
+}
+
+export interface HourlyWeatherItem {
+  time: Date;
+  temperature: number;
+  wind_speed: number;
+  weather_code: number;
+  humidity: number;
+  uv_index: number;
+  surface_pressure: number;
+}
+
+export interface HourlyWeather {
+  items: HourlyWeatherItem[];
 }
