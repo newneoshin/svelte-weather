61882b808c1e23a1996c89c5ae68615c1facaadc
newneoshin <newneoshin@posteo.com>
2026-07-16 16:47:41 +0900

    feat: CityList 구현

---
+++ src/lib/compoments/CityList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { cities } from "../state/cities.svelte";
+  import CityListItem from "./CityListItem.svelte";
+</script>
+
+<ul class="city-list">
+  {#if cities.length === 0}
+    <p class="empty-message">검색해 도시를 추가해보세요.</p>
+  {:else}
+    {#each cities as city (city.id)}
+      <CityListItem {city} />
+    {/each}
+  {/if}
+</ul>
+
+<style>
+  .city-list {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 8px;
+  }
+
+  .empty-message {
+    color: gray;
+    font-size: 12.5px;
+  }
+</style>
