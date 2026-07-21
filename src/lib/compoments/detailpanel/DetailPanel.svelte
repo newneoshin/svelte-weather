4358d898e98c12ce58c0fc2c0898962c2ed17fb0
newneoshin <newneoshin@posteo.com>
2026-07-21 18:42:04 +0900

    refactor: 각 컴포넌트가 state의 날씨 정보들에 의존하도록 변경

--- src/lib/compoments/detailpanel/DetailPanel.svelte
+++ src/lib/compoments/detailpanel/DetailPanel.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
-  import { cities, selection } from "../../state/cities.svelte";
-  import { getWeeklyForecast } from "../../api/weather";
+  import {
+    cities,
+    currentWeatherMap,
+    hourlyForecastMap,
+    selection,
+    weeklyForecastMap,
+  } from "../../state/cities.svelte";
 
-  let city = $derived(
+  const city = $derived(
     cities.find((c) => {
       return c.id == selection.cityId;
     }) ?? null,
   );
 
-  let weeklyPromise = $derived.by(() => {
-    if (city === null) return null;
-    return getWeeklyForecast(city.lat, city.lon);
-  });
+  const currentWeather = $derived(city ? currentWeatherMap[city.id] : null);
+  const hourlyForecast = $derived(city ? hourlyForecastMap[city.id] : null);
+  const weeklyForecast = $derived(city ? weeklyForecastMap[city.id] : null);
 </script>
 
-{#if weeklyPromise === null}
+{#if city === null}
   <span>도시를 선택해주세요.</span>
+{:else if !weeklyForecast}
+  <span>불러오는 중입니다...</span>
 {:else}
-  {#await weeklyPromise}
-    <span>불러오는 중입니다...</span>
-  {:then forecast}
-    <div>
-      <span>{city?.name}</span>
-      <span>최고: {forecast.items[0].temperatureMax}</span>
-      <span>최저: {forecast.items[0].temperatureMin}</span>
-    </div>
-    <div>
-      <span>주간 예보</span>
-      {#each forecast.items as item}
-        <span>날짜: {item.date} </span>
-        <span>날씨: {item.weatherCode} </span>
-        <span>최고: {item.temperatureMax} </span>
-        <span>최저: {item.temperatureMin} </span>
-      {/each}
-    </div>
-  {/await}
+  <div>
+    <span>{city?.name}</span>
+    <span>최고: {weeklyForecast.items[0].temperatureMax}</span>
+    <span>최저: {weeklyForecast.items[0].temperatureMin}</span>
+  </div>
+  <div>
+    <span>주간 예보</span>
+    {#each weeklyForecast.items as item}
+      <span>날짜: {item.date} </span>
+      <span>날씨: {item.weatherCode} </span>
+      <span>최고: {item.temperatureMax} </span>
+      <span>최저: {item.temperatureMin} </span>
+    {/each}
+  </div>
 {/if}
--- src/lib/state/cities.svelte.ts
+++ src/lib/state/cities.svelte.ts
@@ -1,18 +1,38 @@
+import { getAllWeather } from "../api/weather";
 import type { City } from "../types/city";
+import type {
+  CurrentWeather,
+  HourlyForecast,
+  WeeklyForecast,
+} from "../types/weather";
 
 export const cities = $state<City[]>([]);
 export let selection = $state<{ cityId: number | null }>({ cityId: null });
 
-export function addCity(city: City) {
+export const currentWeatherMap = $state<Record<number, CurrentWeather>>({});
+export const hourlyForecastMap = $state<Record<number, HourlyForecast>>({});
+export const weeklyForecastMap = $state<Record<number, WeeklyForecast>>({});
+
+export async function addCity(city: City) {
   if (cities.some((c) => c.id === city.id)) return;
   cities.push(city);
   selection.cityId = city.id;
+
+  const bundle = await getAllWeather(city.lat, city.lon);
+
+  currentWeatherMap[city.id] = bundle.current;
+  hourlyForecastMap[city.id] = bundle.hourly;
+  weeklyForecastMap[city.id] = bundle.weekly;
 }
 
 export function removeCity(city: City) {
   const index = cities.findIndex((c) => c.id === city.id);
   if (index === -1) return;
   cities.splice(index, 1);
+
+  delete currentWeatherMap[city.id];
+  delete hourlyForecastMap[city.id];
+  delete weeklyForecastMap[city.id];
 
   if (selection.cityId === city.id) selection.cityId = null;
 }
